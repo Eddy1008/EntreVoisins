89851e5f3a0b6407563503885bbc289caacfd5c7
Eddy1008 <galmandeddy@gmail.com>
2022-11-01 22:35:48 +0100

    modification du bouton ajout favori selon isFavorite

--- app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailActivity.java
+++ app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourDetailActivity.java
@@ -6,10 +6,12 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -39,6 +41,10 @@ public class NeighbourDetailActivity extends AppCompatActivity {
     TextView mTextViewAboutMe;
     @BindView(R.id.textViewAboutMeInfo)
     TextView mTextviewAboutMeInfo;
+    @BindView(R.id.button_add_favorite)
+    FloatingActionButton addFavoriteButton;
+
+    Neighbour neighbour;
 
 
     @Override
@@ -62,7 +68,7 @@ public class NeighbourDetailActivity extends AppCompatActivity {
 
         Log.d("NeighBourDetailActivity", " NeighbourObject recu: " + myBundle.get("NEIGHBOUR_OBJECT"));
 
-        Neighbour neighbour = (Neighbour) myBundle.get("NEIGHBOUR_OBJECT");
+        neighbour = (Neighbour) myBundle.get("NEIGHBOUR_OBJECT");
         Log.d("NeighBourDetailActivity", " NeighbourObject recu: " + neighbour.getName());
 
         mTextviewNeighbourName.setText(neighbour.getName());
@@ -74,11 +80,33 @@ public class NeighbourDetailActivity extends AppCompatActivity {
                 .load(neighbour.getAvatarUrl())
                 .into(mNeighbourDetailPhoto);
 
+        if(neighbour.isFavorite()) {
+            //Definir apparence du bouton ajouter aux favoris
+            addFavoriteButton.setImageResource(R.drawable.ic_baseline_star_is_favorite_true);
+        } else {
+            //Definir apparence du bouton ajouter aux favoris
+            addFavoriteButton.setImageResource(R.drawable.ic_baseline_star_is_favorite_false);
+        }
     }
+
+
 
     @OnClick(R.id.button_previous_page)
     void previousPage() {
         ListNeighbourActivity.navigate(this);
+    }
+
+    @OnClick(R.id.button_add_favorite)
+    void addToFavoriteList() {
+        if (neighbour.isFavorite() == true) {
+            neighbour.setFavorite(false);
+            Log.d("NeighbourDetailActivity", "bouton ajout favori true devient false : " + neighbour.isFavorite());
+            addFavoriteButton.setImageResource(R.drawable.ic_baseline_star_is_favorite_false);
+        } else {
+            neighbour.setFavorite(true);
+            Log.d("NeighbourDetailActivity", "bouton ajout favori false devient true : " + neighbour.isFavorite());
+            addFavoriteButton.setImageResource(R.drawable.ic_baseline_star_is_favorite_true);
+        }
     }
 
 }
